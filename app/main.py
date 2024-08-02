e339dd9b03a5b46ec7b6bdf07039204935f05434
Geraud KAMENI <geraudkameni@gmail.com>
2024-08-02 22:04:50 +0200

    Handle missing command

--- app/main.py
+++ app/main.py
@@ -5,9 +5,11 @@ def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
     sys.stdout.flush()
-
+    list_commands = set()
     # Wait for user input
-    input()
+    command = input()
+    if command not in list_commands:
+        sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":

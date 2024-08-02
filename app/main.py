ac870e86ccbe3cf49ef35d5289e2457d118cd794
Geraud KAMENI <geraudkameni@gmail.com>
2024-08-02 23:31:48 +0200

    echo builtn

--- app/main.py
+++ app/main.py
@@ -4,18 +4,23 @@ import sys
 def main():
     # Uncomment this block to pass the first stage
     
-    list_commands = set("")
+    list_commands = set("echo")
     # Wait for user input
 
     while True: 
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        command = input()
+        user_input = input()
+        command = user_input.split(" ")[0]
+        args = user_input.split(" ")[1:]
         if command == "exit 0":
             break
-        if command not in list_commands:
+        elif command == "echo":
+            sys.stdout.write(" ".join(args))
+            sys.stdout.write("\n")
+        elif command not in list_commands:
             sys.stdout.write(f"{command}: command not found\n")
-
+    
 
 if __name__ == "__main__":
     main()

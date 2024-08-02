b8144bd20f21fdc2eeb65a123f5fc51ba396ed0d
Geraud KAMENI <geraudkameni@gmail.com>
2024-08-02 22:14:47 +0200

    REPL

--- app/main.py
+++ app/main.py
@@ -3,13 +3,16 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    
     list_commands = set()
     # Wait for user input
-    command = input()
-    if command not in list_commands:
-        sys.stdout.write(f"{command}: command not found\n")
+
+    while True: 
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        command = input()
+        if command not in list_commands:
+            sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":

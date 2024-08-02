27d6f926806902400b42b28a45e9998ec542dfbc
Geraud KAMENI <geraudkameni@gmail.com>
2024-08-02 22:23:07 +0200

    REPL

--- app/main.py
+++ app/main.py
@@ -4,13 +4,15 @@ import sys
 def main():
     # Uncomment this block to pass the first stage
     
-    list_commands = set()
+    list_commands = set("")
     # Wait for user input
 
     while True: 
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input()
+        if command == "exit 0":
+            break
         if command not in list_commands:
             sys.stdout.write(f"{command}: command not found\n")
 

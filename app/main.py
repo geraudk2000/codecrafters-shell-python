f76dfafd19c6adfb223c1f7e0dfb40a4f13c33e2
Geraud KAMENI <geraudkameni@gmail.com>
2024-08-03 13:48:50 +0200

    The type builin: excutable

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import sys
+import os
 
 
 def main():
     # Uncomment this block to pass the first stage
     
     list_commands = set(["echo", "type", "exit"])
-
+    PATH = os.environ.get('PATH')
     # Wait for user input
 
     while True: 
@@ -14,6 +15,9 @@ def main():
         user_input = input()
         command = user_input.split(" ")[0]
         args = user_input.split(" ")[1:]
+        paths = PATH.split(":")
+        path_found = False
+        
         if user_input == "exit 0":
             break
         elif command == "echo":
@@ -21,12 +25,13 @@ def main():
             sys.stdout.write("\n")
         elif command == "type": 
             cmd = args[0]
-            if cmd in list_commands:
-                sys.stdout.write(f"{cmd} is a shell builtin" )
-                sys.stdout.write("\n")
-            else: 
-                sys.stdout.write(f"{cmd}: not found" )
-                sys.stdout.write("\n")
+            for path in paths:
+                if os.path.isfile(f"{path}/{cmd}"):
+                    sys.stdout.write(f"{cmd} is {path}/{cmd}\n" )
+                    path_found = True
+            if not path_found:
+                sys.stdout.write(f"{cmd}: command not found\n")
+
         elif command not in list_commands:
             sys.stdout.write(f"{command}: command not found\n")
     
